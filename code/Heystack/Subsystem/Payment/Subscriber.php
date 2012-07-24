8ed91f738366ba988e9919a24faea2aac77a2d2a
glenn-bautista <glenn@heyday.co.nz>
2012-07-25 09:48:12 +1200

    UPDATE: added documentation

--- code/Heystack/Subsystem/Payment/Subscriber.php
+++ code/Heystack/Subsystem/Payment/Subscriber.php
@@ -1,5 +1,13 @@
 <?php
+/**
+ * This file is part of the Ecommerce-Vouchers package
+ *
+ * @package Ecommerce-Payment
+ */
 
+/**
+ * Payment namespace
+ */
 namespace Heystack\Subsystem\Payment;
 
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -7,25 +15,59 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 use Heystack\Subsystem\Ecommerce\Transaction\Events as TransactionEvents;
 use Heystack\Subsystem\Ecommerce\Transaction\Event\TransactionStoredEvent;
+use Heystack\Subsystem\Ecommerce\Currency\CurrencyService;
 
 use Heystack\Subsystem\Core\State\State;
 
 
 use Heystack\Subsystem\Payment\Interfaces\PaymentHandlerInterface;
 
+/**
+ * Transaction's Subscriber
+ * 
+ * Handles both subscribing to events and acting on those events needed for the PaymentHandler work properly
+ *
+ * @copyright  Heyday
+ * @author Glenn Bautista <glenn@heyday.co.nz>
+ * @package Ecommerce-Core
+ */
 class Subscriber implements EventSubscriberInterface
 {
+    /**
+     * Holds the Event Dispatcher service
+     * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface 
+     */
     protected $eventService;
+    
+    /**
+     * Holds the PaymentHandler service
+     * @var \Heystack\Subsystem\Payment\Interfaces\PaymentHandlerInterface 
+     */
     protected $paymentHandler;
+    
+    /**
+     * Holds the State service
+     * @var \Heystack\Subsystem\Core\State\State 
+     */
     protected $state;
 
+    /**
+     * Creates the Subscriber object
+     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventService
+     * @param \Heystack\Subsystem\Payment\Interfaces\PaymentHandlerInterface $paymentHandler
+     * @param \Heystack\Subsystem\Core\State\State $state
+     */
     public function __construct(EventDispatcherInterface $eventService, PaymentHandlerInterface $paymentHandler, State $state)
     {
         $this->eventService = $eventService;
         $this->paymentHandler = $paymentHandler;
         $this->state = $state;
     }
-
+    
+    /**
+     * Returns an array of events to subscribe to and the methods to call when those events are fired
+     * @return array
+     */
     public static function getSubscribedEvents()
     {
         return array(
@@ -34,14 +76,22 @@ class Subscriber implements EventSubscriberInterface
         );
     }
 
+    /**
+     * Called after the Transaction is stored, signals that the payment handler needs to execute the payment
+     * @param \Heystack\Subsystem\Ecommerce\Transaction\Event\TransactionStoredEvent $event
+     */
     public function onTransactionStored(TransactionStoredEvent $event)
     {
         $this->paymentHandler->executePayment($event->getTransactionID());
     }
     
+    /**
+     * Called after the payment is successfully completed.
+     * Clears everything in state except the active currency.
+     */
     public function onPaymentSuccessful()
     {
-        $this->state->removeAll(array(\Heystack\Subsystem\Ecommerce\Currency\CurrencyService::STATE_KEY));
+        $this->state->removeAll(array(CurrencyService::STATE_KEY));
     }
 
 }
--- code/Heystack/Subsystem/Payment/Traits/PaymentConfigTrait.php
+++ code/Heystack/Subsystem/Payment/Traits/PaymentConfigTrait.php
@@ -1,11 +1,36 @@
 <?php
+/**
+ * This file is part of the Ecommerce-Payment package
+ *
+ * @package Ecommerce-Payment
+ */
 
+/**
+ * Interfaces namespace
+ */
 namespace Heystack\Subsystem\Payment\Traits;
 
+/**
+ * Provides an implementation of setting and getting the configuration for use on a PaymentHandler class
+ *
+ * @copyright  Heyday
+ * @author Glenn Bautista <glenn@heyday.co.nz>
+ * @package Ecommerce-Payment
+ */
 trait PaymentConfigTrait
 {
+    /**
+     * Defines an array of required parameters used in setConfig
+     * @return array
+     */    
     abstract protected function getRequiredConfigParameters();
     
+    /**
+     * Sets an array of config parameters onto the data array. 
+     * Checks to see if all the required parameters are present.
+     * @param array $config
+     * @throws \Exception
+     */
     public function setConfig(array $config)
     {
         $missing = array_diff($this->getRequiredConfigParameters(), array_keys($config));
@@ -19,6 +44,10 @@ trait PaymentConfigTrait
         }
     }
     
+    /**
+     * Retrieves the configuration array
+     * @return array
+     */
     public function getConfig()
     {
         return isset($this->data[self::CONFIG_KEY]) ? $this->data[self::CONFIG_KEY] : null;
