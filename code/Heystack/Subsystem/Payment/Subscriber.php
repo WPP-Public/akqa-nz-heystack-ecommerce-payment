4d8d0b491c1a9cf674273360f1982081295bcc8b
glenn-bautista <glenn@heyday.co.nz>
2012-07-24 14:27:03 +1200

    FEATURE: Clear state once payment is successful

--- code/Heystack/Subsystem/Payment/Subscriber.php
+++ code/Heystack/Subsystem/Payment/Subscriber.php
@@ -8,6 +8,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Heystack\Subsystem\Ecommerce\Transaction\Events as TransactionEvents;
 use Heystack\Subsystem\Ecommerce\Transaction\Event\TransactionStoredEvent;
 
+use Heystack\Subsystem\Core\State\State;
+
 
 use Heystack\Subsystem\Payment\Interfaces\PaymentHandlerInterface;
 
@@ -15,23 +17,31 @@ class Subscriber implements EventSubscriberInterface
 {
     protected $eventService;
     protected $paymentHandler;
+    protected $state;
 
-    public function __construct(EventDispatcherInterface $eventService, PaymentHandlerInterface $paymentHandler)
+    public function __construct(EventDispatcherInterface $eventService, PaymentHandlerInterface $paymentHandler, State $state)
     {
         $this->eventService = $eventService;
         $this->paymentHandler = $paymentHandler;
+        $this->state = $state;
     }
 
     public static function getSubscribedEvents()
     {
         return array(
-            TransactionEvents::STORED  => array('onTransactionStored')
+            TransactionEvents::STORED  => array('onTransactionStored'),
+            Events::SUCCESSFUL => array('onPaymentSuccessful')
         );
     }
 
     public function onTransactionStored(TransactionStoredEvent $event)
     {
         $this->paymentHandler->executePayment($event->getTransactionID());
+    }
+    
+    public function onPaymentSuccessful()
+    {
+        $this->state->removeAll(array(\Heystack\Subsystem\Ecommerce\Currency\CurrencyService::STATE_KEY));
     }
 
 }
